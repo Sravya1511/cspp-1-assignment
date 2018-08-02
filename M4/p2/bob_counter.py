fd4b157cb55098d065c77ffcd2dedad847e1df9a
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-02 15:09:22 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

---
+++ M4/p2/bob_counter.py
@@ -0,0 +1,13 @@
+'''Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s. 
+
+Number of times bob occurs is: 2'''
+
+def main():
+"""count"""
+	S_A = input("Enter a string ")
+    S_B = input("Enter a sub string ")
+    print(S_A.count(S_B))
+if __name__ == "__main__":
+	main()
