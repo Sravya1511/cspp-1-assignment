314e86230d7a49a5d8690dd57fabf3592a0c67a8
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 17:41:46 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -1,15 +1,15 @@
 """bisect"""
 def main():
     """bisection"""
-    x_ = int(input())
+    x_q = int(input())
     epsilon_ = 0.01
     numuesses_ = 0
     low_ = 0.0
-    high_ = x_
+    high_ = x_q
     ans_ = (high_ + low_)/2.0
-    while abs(ans_**2 - x_) >= epsilon_:
+    while abs(ans_**2 - x_q) >= epsilon_:
         numuesses_ += 1
-        if ans_**2 < x_:
+        if ans_**2 < x_q:
             low_ = ans_
         else:
             high_ = ans_
