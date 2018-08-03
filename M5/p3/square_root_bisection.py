a05b88cde6a43c94ecbab6742d9c8222faba085f
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 16:59:07 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -1,15 +1,17 @@
 """ Write a python program to find the square root of the given number"""
 def main():
-"""bisection"""
-x_1 = int(input())
-epsilon_1 = 0.01
-low_1 = 0.0
-high_1 = x_1
-ans_1 = (high_1 + low_1)/2.0
-while abs(ans_1**2 - x_1) >= epsilon_1:
-    if ans_1**2 < x_1:
-        low_1 = ans_1
-    else:
-        high_1 = ans_1
+    """bisection"""
+    x_1 = int(input())
+    epsilon_1 = 0.01
+    low_1 = 0.0
+    high_1 = x_1
     ans_1 = (high_1 + low_1)/2.0
-print(str(ans_1))
+    while abs(ans_1**2 - x_1) >= epsilon_1:
+        if ans_1**2 < x_1:
+            low_1 = ans_1
+        else:
+            high_1 = ans_1
+    ans_1 = (high_1 + low_1)/2.0
+    print(str(ans_1))
+if __name__ == "__main__":
+    main()
