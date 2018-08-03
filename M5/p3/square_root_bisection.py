1d65c4383535a9bdcdf6b78e54a571d3cf5b7ae1
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 17:30:42 +0530

    " -> 0 of 3 passed. pylint: 8.57/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -1,4 +1,4 @@
-"""Write a python program to find the square root of the given number"""
+
 def main():
     """bisection"""
     x_1 = int(input())
@@ -12,6 +12,6 @@ def main():
         else:
             high_1 = ans_1
     ans_1 = (high_1 + low_1)/2.0
-    print(str(ans_1)
+    print(str(ans_1))
 if __name__ == "__main__":
 	main()
