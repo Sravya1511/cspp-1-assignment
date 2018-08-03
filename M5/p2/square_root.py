244fa1b202b312011c265fb5a58cce2e43258365
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 17:18:06 +0530

    " -> 0 of 1 passed. pylint: 9.17/10 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -1,16 +1,15 @@
 """ Write a python program to find the square root of the given number """
 def main():
     """root"""
-    S_1 = int(input())
+    s_1 = int(input())
     epsilon_1 = 0.01
     guess_1 = 0
     incre_1 = 0.001
-    while(abs(guess_1**2 - S_1) >= epsilon_1 and guess_1 <= S_1):
+    while(abs(guess_1**2 - s_1) >= epsilon_1 and guess_1 <= s_1):
         guess_1 += incre_1
-    if abs(guess_1**2 - S_1 >= epsilon_1):
+    if abs(guess_1**2 - s_1 >= epsilon_1):
         print("No square")
     else:
         print("sqaure" + str(guess_1))
 if __name__== "__main__":
-	main()
-	
+    main()
