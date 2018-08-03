6f26eb67b33cf087febd4fec05fa239742eaed70
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 16:53:32 +0530

    " -> 3 of 3 passed. pylint: 0.91/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -8,19 +8,15 @@
 # testcase 2
 # input: 49
 # output: 6.999999999999991
-
-def main():
-    x_1 = int(input())
-    approx_1 = 0.01
-    low_1 = 1
-    high_1 = x_1
-    guess_1 = (low_1 + high_1)/2
-    while guess_1**2 - x_1 >= approx_1:
-        if guess_1**2 < x_1:
-            low_1 = guess_1
-        if guess_1**2 > x_1:
-            high_1 = guess_1
-        guess_1 = (low_1 + high_1)/2
-    print("square root is " + str(guess_1))    
-if __name__== "__main__":
-	main()
+x_1 = int(input())
+epsilon_1 = 0.01
+low_1 = 0.0
+high_1 = x_1
+ans_1 = (high_1 + low_1)/2.0
+while abs(ans_1**2 - x_1) >= epsilon_1:
+    if ans_1**2 < x_1:
+        low_1 = ans_1
+    else:
+        high_1 = ans_1
+    ans_1 = (high_1 + low_1)/2.0
+print(str(ans_1))
