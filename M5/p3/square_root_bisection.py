6edbfa7b1d98ab41f046a0f4ed874a85637d6974
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 16:54:50 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -1,13 +1,6 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
+""" Write a python program to find the square root of the given number"""
+def main():
+"""bisection"""
 x_1 = int(input())
 epsilon_1 = 0.01
 low_1 = 0.0
