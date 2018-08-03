870543babbc3bff6bccdefc532bd5e3f3c6488bd
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 18:39:06 +0530

    " -> 3 of 3 passed. pylint: 9.33/10 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -1,18 +1,18 @@
-""" Write a python program to find the square root of the given number """
+"""square"""
 def main():
     """square"""
-    x_1 = int(input())
+    square_ = int(input())
     epsilon_ = 0.01
-    cla_ = 0
     guess_ = 0.0
-    increment_ = 0.0001
+    cla_ = 0
+    increment_ = 0.1
     num_guesses = 0
-    while abs(guess_**2 - x_1) >= epsilon_:
+    while abs(guess_**2 - square_) >= epsilon_:
         guess_ += increment_
         num_guesses += 1
-    if abs(guess_**2 - x_1) >= epsilon_:
-        cla_ = cla_ + 1
+    if abs(guess_**2 - square_) >= epsilon_:
+        cla_ = cla_+1
     else:
         print(str(guess_))
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
