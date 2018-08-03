ffc2c4ce75871a562e79bbcf2e400d4b90e06a10
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-03 17:59:04 +0530

    " -> 3 of 3 passed. pylint: 8.18/10 "

--- M5/p4/square_root_newtonrapson.py
+++ M5/p4/square_root_newtonrapson.py
@@ -1,14 +1,13 @@
 """Write a python program to find the square root of the given number"""
 def main():
     """nr"""
-	step_ = 0.01
+    step_ = 0.01
     y_ = int(input())
     guess_ = y_/2.0
     nog_ = 0
     while abs(guess_*guess_ - y_) >= step_:
         nog_ += 1
-        guess_ = guess_ - (((guess_**2) - Y_)/(2*guess_))
-    print('numGuesses = ' + str(nog_))
-    print('Square root of ' + str(y_) + ' is about ' + str(guess_))
+        guess_ = guess_ - (((guess_**2) - y_)/(2*guess_))
+    print(str(guess_))
 if __name__== "__main__":
     main()
