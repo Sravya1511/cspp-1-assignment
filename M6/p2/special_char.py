444a408e9214069a4227773e69364d2c6f189c25
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-04 10:41:47 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- M6/p2/special_char.py
+++ M6/p2/special_char.py
@@ -4,6 +4,7 @@ example : ab!@#cd is the input, the output is ab   cd
 Output has three spaces, which are to be replaced with these special characters
 '''
 def main():
+	"""special"""
     str_input = input()
     for letter in str_input:
         if letter in '!' '@' '#' '$' '%' '^' '&' '*':
