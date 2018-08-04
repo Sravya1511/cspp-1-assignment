2482d1596bce0485d86c556cdb3ca2c36ead1338
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-04 10:40:32 +0530

    " -> 1 of 7 passed. pylint: 8.75/10 "

--- M6/p2/special_char.py
+++ M6/p2/special_char.py
@@ -9,6 +9,6 @@ def main():
         if letter in '!' '@' '#' '$' '%' '^' '&' '*':
             print(" ")
         else:
-            print(str(letter)
+            print(str(letter))
 if __name__ == "__main__":
     main()
