f13fc89d528e8f777aa62e2213afeff686bd1357
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-04 16:05:04 +0530

    version

--- M5/GuessMyNumber/guess_my_number.py
+++ M5/GuessMyNumber/guess_my_number.py
@@ -1,8 +1,19 @@
-#Guess My Number Exercise
-
+"""guess"""
 def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+    """guess my number"""
+    mid_n = 50
+    high_n = 100
+    low_n = 0
+    inp_n = 'l'
+    while inp_n != 'c':
+        print(mid_n)
+        inp_n = input("Enter h or l or c")
+        if inp_n == 'h':
+            high_n = mid_n
+            mid_n = (high_n + low_n)//2
+        elif inp_n == 'l':
+            low_n = mid_n
+            mid_n = (high_n + low_n)//2
+    print('your guess number is :', mid_n)
+if __name__ == "__main__":
+    main()
