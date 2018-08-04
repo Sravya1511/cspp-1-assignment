e4feec77efe15a1b448704089414a423a95e43a9
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-04 11:28:43 +0530

    " -> 7 of 7 passed. pylint: 9.00/10 "

--- M6/p2/special_char.py
+++ M6/p2/special_char.py
@@ -6,10 +6,12 @@ Output has three spaces, which are to be replaced with these special characters
 def main():
     """special"""
     str_input = input()
+    st1 = ""
     for letter in str_input:
         if letter in '!' '@' '#' '$' '%' '^' '&' '*':
-            break
+            st1 = st1 + " "   
         else:
-            print(str(letter))
+            st1 = st1+ letter
+    print(st1)
 if __name__ == "__main__":
     main()
