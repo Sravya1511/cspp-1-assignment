0a8ba2fb20ea8988dca52c2691d44eda789496ce
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-06 15:02:23 +0530

    version

---
+++ M7/Functions - Odd Exercise/odd.py
@@ -0,0 +1,12 @@
+"""even"""
+def odd_1(x_1):
+    """even"""
+    if x_1%2 == 0:
+        return False
+    return True
+def main():
+    """even"""
+    data_1 = input()
+    print(odd_1(int(data_1)))
+if __name__ == "__main__":
+    main()
