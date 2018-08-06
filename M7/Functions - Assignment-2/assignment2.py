a8dc7f978d214b533c3ed5bdf3f296b360b4cae2
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-06 19:56:10 +0530

    " -> 10 of 10 passed. pylint: 6.50/10 "

--- M7/Functions - Assignment-2/assignment2.py
+++ M7/Functions - Assignment-2/assignment2.py
@@ -1,28 +1,28 @@
 # Assignment-2 - Paying Debt off in a Year
 def payingDebtOffInAYear(balance_1, annual_interest_rate):
-	mfmp_ = 0
-	temp_bal = balance_1
-	year_count = 1
-	while balance_1 > 0:
-		mfmp_ += 10
-		balance_1 = temp_bal
+    """codE"""
+    mfmp_ = 0
+    temp_bal = balance_1
+    year_count = 1
+    while balance_1 > 0:
+        mfmp_ += 10
+        balance_1 = temp_bal
 		# print(mfmp_,year_count)
-		for i in range(1, 13):
-			m_intrest_rate = annual_interest_rate/12.0
-			monthly_unpaid_balance = balance_1 - mfmp_
-			balance_1 = monthly_unpaid_balance + m_intrest_rate*monthly_unpaid_balance
+        for i in range(1, 13):
+            m_intrest_rate = annual_interest_rate/12.0
+            monthly_unpaid_balance = balance_1 - mfmp_
+            balance_1 = monthly_unpaid_balance + m_intrest_rate*monthly_unpaid_balance
 			# balance_1 = updated_balance_each_month
 			# print (i,balance_1)
 		# if balance_1 <=0.5:
 		#     break
-		
-		year_count += 1
-	return mfmp_
+        year_count += 1
+    return mfmp_
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	print("Lowest Payment:",payingDebtOffInAYear(data[0],data[1]))
-	
+    """code"""
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print("Lowest Payment:",payingDebtOffInAYear(data[0],data[1]))
 if __name__ == "__main__":
 	main()
