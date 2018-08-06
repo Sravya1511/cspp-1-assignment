7fedc5104d40e0a2eeec111453ec72aca8ed9e19
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-06 19:46:58 +0530

    " -> 0 of 10 passed. pylint: -3.00/10 "

--- M7/Functions - Assignment-2/assignment2.py
+++ M7/Functions - Assignment-2/assignment2.py
@@ -1,6 +1,6 @@
 # Assignment-2 - Paying Debt off in a Year
 def payingDebtOffInAYear(balance_1, annual_interest_rate):
-	mfmp_ = 0
+	mfmp_ = 10
 	temp_bal = balance_1
 	year_count = 1
 	while balance_1 > 0:
@@ -9,8 +9,8 @@ def payingDebtOffInAYear(balance_1, annual_interest_rate):
 		for i in range(1, 13):
 			m_intrest_rate = annual_interest_rate/12.0
 			monthly_unpaid_balance = balance_1 - mfmp_
-			updated_balance_each_month = monthly_unpaid_balance + m_intrest_rate*monthly_unpaid_balance
-			balance_1 = updated_balance_each_month
+			balance_1 = monthly_unpaid_balance + m_intrest_rate*monthly_unpaid_balance
+			# balance_1 = updated_balance_each_month
 			# print (i,balance_1)
 		# if balance_1 <=0.5:
 		#     break
