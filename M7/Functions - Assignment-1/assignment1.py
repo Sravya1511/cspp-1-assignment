1159e19d3814139870200cdeb965c80991279d70
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-06 17:54:39 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

---
+++ M7/Functions - Assignment-1/assignment1.py
@@ -0,0 +1,17 @@
+"""assignment"""
+def paying_debt_off_in_year(balance_1, annual_interest_rate, monthly_payment_rate):
+    """A"""
+    for i in range(1,13):
+        monthly_intrest_rate = annual_interest_rate/12.0
+	    minimum_monthly_payment = monthly_payment_rate x balance_1
+	    monthly_unpaid_balance = balance_1 - minimum_monthly_payment
+	    updated_balance_each_month =  monthly_unpaid_balance + monthly_intrest_rate*monthly_unpaid_balance
+        balance_1 = updated_balance_each_month
+    return round (balance_1, 2)
+def main():
+	data = input()
+	data = data.split(' ')
+	data = list(map(float, data))
+	print("Remaining balance: ",paying_debt_off_in_a_year(data[0],data[1],data[2]))
+if __name__ == "__main__":
+	main()
