bafd341cc86b1df282bb31a7d6d2a7c46cd6c9d7
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-08 10:34:47 +0530

    version

---
+++ M9/Odd Tuples Exercise/odd_tuples.py
@@ -0,0 +1,21 @@
+"""tuple"""
+def oddTuples(aTup):
+    """tuple"""
+    odd = ()
+    l = len(aTup)
+    j= 0
+    while j < l:
+        odd = odd + (aTup[j],)
+        j = j+2
+    return odd
+def main():
+    """tuple"""
+    data = input()
+    data = data.split()
+    aTup = ()
+    for j in range(len(data)):
+        aTup += (int(data[j]),)
+    print(oddTuples(aTup))
+if __name__ == "__main__":
+    main()
+    
