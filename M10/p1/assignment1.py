7efbec61fbea0f0c6557cc7cc76864891bdcfdc3
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-09 16:53:54 +0530

    " -> 10 of 10 passed. pylint: 7.14/10 "

--- M10/p1/assignment1.py
+++ M10/p1/assignment1.py
@@ -5,28 +5,33 @@ a list of letters, letters_guessed. This function returns a string
 that is comprised of lowercase English letters - all lowercase English letters
 that are not in letters_guessed
 '''
-def get_available_letters(letters_guessed):
-    import string
-    li = list(string.ascii_lowercase)
-    for i in letters_guessed:
-        if i in li:
-            li.remove(i)
-    return "".join(li)
 # def get_available_letters(letters_guessed):
 #     import string
-#     key = list(string.ascii_lowercase)
-#     value = key
-#     dictionary = dict(zip(key, value))
-#     for i in range(len(letters_guessed)):
-#         del value[i]
-#         print(value)
-#     return value
+#     li = list(string.ascii_lowercase)
+#     for i in letters_guessed:
+#         if i in li:
+#             li.remove(i)
+#     return "".join(li)
+def get_available_letters(letters_guessed):
+    import string
+    x = ""
+    key = list(string.ascii_lowercase)
+    value = key
+    dictionary = dict(zip(key, value))
+    for i in letters_guessed:
+         if i in dictionary.values():
+            del dictionary[i]
+    for v in dictionary.values():
+        x = x+v
+    return x
+
+           
 def main():
     '''
     Main function for the given program
     '''
     user_input = input()
-    user_input = user_input.split()
+    user_input = user_input.split(" ")
     data = []
     for char in user_input:
         data.append(char)
