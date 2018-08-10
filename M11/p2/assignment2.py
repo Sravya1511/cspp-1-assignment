0ca1c80cc2e25cc37e7b60b4e0816842d4fcfb90
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 18:24:22 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M11/p2/assignment2.py
+++ M11/p2/assignment2.py
@@ -1,5 +1,4 @@
 #Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
 def updateHand(hand, word):
     """a2"""
     handnew = dict(hand)
