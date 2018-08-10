db56c7f5a7df2ba3f1d0e149b919f9fe1fcb6356
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 20:03:02 +0530

    " -> 0 of 9 passed. pylint: 3.68/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -10,18 +10,16 @@ def get_word_score(word, n):
     #     x+=1
     # dictionary_ = dict(zip(key, value))
     # print(dictionary_)
-    dictionary_ = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    dictionary_ = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     length_1 = len(word)
     if length <= n:
         for i in word:
             if i in dictionary_.keys():
-                sum = sum+dictionary_[i]
-        sum = sum*length_1
+                sum_1 = sum_1 + dictionary_[i]
+        sum_1 = sum_1*length_1
         if n == length_1:
-            sum += 50
-        return sum
+            sum_1 += 50
+        return sum_1
     else:
         print("worng inputs")
 def main():
