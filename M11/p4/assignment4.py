d34cd6a51f1693ba765d1a32efa37797fc9ebfe9
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 19:33:43 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- M11/p4/assignment4.py
+++ M11/p4/assignment4.py
@@ -7,12 +7,14 @@ def calculate_hand_len(hand_1):
     return sum_1
 def main():
     """sum"""
-    n_1 = input()
+    n_1 = int(input())
     adict = {}
-    for i_1 in range(int(n_1)):
+    #for i_1 in range(int(n_1)):
+    while i_1 < n_1
         data = input()
         l_1 = data.split()
         adict[l_1[0]] = int(l_1[1])
+        i_1 += 1
     print(calculate_hand_len(adict))
 if __name__ == "__main__":
     main()
