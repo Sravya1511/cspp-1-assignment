a9b504c5731b1b0ed31667a0de4b21f488d2c7ee
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 20:04:30 +0530

    " -> 0 of 9 passed. pylint: 1.58/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -12,12 +12,12 @@ def get_word_score(word, n):
     # print(dictionary_)
     dictionary_ = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     length_1 = len(word)
-    if length_1 <= n:
+    if length_1 <= n_1:
         for i in word:
             if i in dictionary_.keys():
                 sum_1 = sum_1 + dictionary_[i]
         sum_1 = sum_1*length_1
-        if n == length_1:
+        if n_1 == length_1:
             sum_1 += 50
         return sum_1
     else:
