5af823f71805872a62eb06a50fb16dbe8c4ee8fa
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 19:28:36 +0530

    " -> 0 of 5 passed. pylint: 4.00/10 "

--- M11/p4/assignment4.py
+++ M11/p4/assignment4.py
@@ -1,15 +1,15 @@
-#Exercise: Assignment-4
+"""Exercise: Assignment-4"""
 def calculateHandlen(hand):
     """sum"""
-    sum = 0
-    for i in hand:
-        sum = sum + hand[i]
-    return sum
+    sum_1 = 0
+    for i_1 in hand:
+        sum_1 = sum_1 + hand[i_1]
+    return sum_1
 def main():
     """sum"""
-    n = input()
+    n_1 = input()
     adict = {}
-    for i in range(int(n)):
+    for i_1 in range(int(n)):
         data = input()
         l = data.split()
         adict[l[0]] = int(l[1])
