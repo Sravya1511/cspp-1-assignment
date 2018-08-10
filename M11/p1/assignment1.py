63c49cdfbf501096ef62fa04a822b464b2491ac2
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 20:18:24 +0530

    " -> 9 of 9 passed. pylint: 8.24/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -11,7 +11,7 @@ def get_word_score(word_1, n_1):
     #     x+=1
     # dictionary_ = dict(zip(key, value))
     # print(dictionary_)
-    SCRABBLE_LETTER_VALUES = {
+    dictionary_ = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
