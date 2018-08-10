9a164fa7b823221b616d8d5abca4e64bd1e2cde8
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 19:52:57 +0530

    " -> 0 of 6 passed. pylint: 0/0 "

--- M11/p2/assignment2.py
+++ M11/p2/assignment2.py
@@ -1,45 +1,30 @@
 #Exercise: Assignment-2
-def updateHand(hand, word):
+def update_hand(hand_1, word_1):
     """a2"""
-    handnew = dict(hand)
-    key = []
-    value = []
-    for i in word:
-        if i in hand.keys():
+    hand_new = dict(hand_1)
+    key_1 = []
+    value_1 = []
+    for i in word_1:
+        if i in hand_1.keys():
             # key.append(i)
             # val = hand[i] - 1
             # value.append(val)
-            handnew[i] = handnew[i] - 1
+            hand_new[i] = hand_new[i] - 1
         # else:
         #     handnew[i] = handnew[i]
     #         key.append(i)
     #         value.append(hand[i])
     # hand = dict(zip(key, value))
-    return(handnew)
-    """
-    Assumes that 'hand' has all the letters in word.
-    In other words, this assumes that however many times
-    a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
-
-    Updates the hand: uses up the letters in the given word
-    and returns the new hand, without those letters in it.
-
-    Has no side effects: does not modify hand.
-
-    word: string
-    hand: dictionary (string -> int)    
-    returns: dictionary (string -> int)
-    """
-    # TO DO ... <-- Remove this comment when you code this function
+    return(hand_new)
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split(" ")
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-if __name__== "__main__":
+    """string"""
+	n_1 = input()
+	adict_1 = {}
+	for i in range(int(n_1)):
+		data = input()
+		l = data.split(" ")
+		adict_1[l[0]] = int(l[1])
+	data1 = input()
+	print(update_hand(adict_1, data1))
+if __name__ == "__main__":
 	main()
