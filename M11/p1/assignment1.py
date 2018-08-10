dd6e7a52ccbbc8f5ab9fc01e90972f96d56a06d2
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-10 20:17:43 +0530

    " -> 0 of 9 passed. pylint: 1.18/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -11,7 +11,10 @@ def get_word_score(word_1, n_1):
     #     x+=1
     # dictionary_ = dict(zip(key, value))
     # print(dictionary_)
-    dictionary_ = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     length_1 = len(word_1)
     # if length_1 <= n_1:
     for i in word_1:
