15bb097b60463cd580eefdf33e5c2f7bda7cfb7e
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-11 11:35:46 +0530

    " -> 0 of 3 passed. pylint: 3.70/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -1,38 +1,33 @@
-'''
-    Assignment-1 Create Social Network
-'''
-
+"""Assignment-1 Create Social Network"""
 def create_social_network(data):
-    list1 = data.split (' ')
-    key = []
-    value = []
-    list2 = []
+    """split"""
+    list_1 = data.split (' ')
+    key_1 = []
+    value_1 = []
     dictionary_ = {}
     x = 1
     j = 0
     # return list1
     # for i in range(len(list1)):
-    for i in range(len(list1)):
-        if list1[i] == "follows":
-            key.append(list1[i-1])
+    for i in range(len(list_1)):
+        if list_1[i] == "follows":
+            key_1.append(list_1[i-1])
             # value.append(list1[j+1])
     # for i in range(len(list1)-1):
-    for i in range(len(list1)):
-        if list1[i] == "follows":
+    for i in range(len(list_1)):
+        if list1_1[i] == "follows":
             j = i+1
-            value.append(list1[j])
+            value.append(list_1[j])
         #     print(j)
         #     while list1[j] != key[x]:
         #         value.append(list1[j+1])
         #         j = j+1
         #     x = x+1
         # i = j
-    dictionary_ = dict(zip(key, value))
+    dictionary_ = dict(zip(key_1, value_1))
                 # if i == len(list1)
                 # i = i+1
-    return dictionary_
-
-                
+    return dictionary_                
     '''
         The data argument passed to the function is a string
         It represents simple social network data
@@ -63,9 +58,6 @@ def create_social_network(data):
     '''
 
     # remove the pass below and start writing your code
-    import string
-
-
 def main():
     '''
         handling testcase input and printing output
@@ -76,8 +68,6 @@ def main():
         i += 1
         string += input()
         string += '\n'
-
     print(create_social_network(string))
-
 if __name__ == "__main__":
     main()
