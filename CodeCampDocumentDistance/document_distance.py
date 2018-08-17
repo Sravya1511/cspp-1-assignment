9266794416be8558048d3219feab571213d75e37
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-17 18:15:42 +0530

    " -> 0 of 3 passed. pylint: 0.00/10 "

--- CodeCampDocumentDistance/document_distance.py
+++ CodeCampDocumentDistance/document_distance.py
@@ -4,61 +4,85 @@
 import math
 
 def similarity(dict1, dict2):
-    dic = {}
-    str1 = str(dict1)
-    str2 = str(dict2)
-    str1.strip()
-    str2.strip()
-    list1 = str1.lower().split()
-    list2 = str2.lower().split()
+    # dic = {}
+    # str1 = str(dict1)
+    # str2 = str(dict2)
+    # str1.strip()
+    # str2.strip()
+    # list1 = str1.lower().split()
+    # list2 = str2.lower().split()
    
     
 
-    stopwords = load_stopwords("stopwords.txt")
-    for i in list1:
-        if i in stopwords:
-            list1.remove(i)
-    # print(list1)
-    for i in list2:
-        if i in stopwords:
-            list2.remove(i)
-            
-    # print(list2)
-    # print(list1)
-    # print(list2)
-    list3 = list1 + list2
-    # print(list3)
+    # stopwords = load_stopwords("stopwords.txt")
     # for i in list1:
-    #     if i not in dic:
-    #         dic[i] = [list1.count(i)]
-    # print(dic)
+    #     if i in stopwords:
+    #         list1.remove(i)
+    # # print(list1)
     # for i in list2:
-    #     if i in dic:
+    #     if i in stopwords:
+    #         list2.remove(i)
 
-    #         dic[i].append(str(list2.count(i)))
+    # # print(list2)
+    # # print(list1)
+    # # print(list2)
+    # list3 = list1 + list2
+    # # print(list3)
+    # # for i in list1:
+    # #     if i not in dic:
+    # #         dic[i] = [list1.count(i)]
+    # # print(dic)
+    # # for i in list2:
+    # #     if i in dic:
+
+    # #         dic[i].append(str(list2.count(i)))
+    # #     if i not in dic:
+    # #         dic[i] = [list2.count(i)]
+    # # print(dic)
+    # for i in list3:
     #     if i not in dic:
-    #         dic[i] = [list2.count(i)]
-    # print(dic)
-    for i in list3:
-        if i not in dic:
-            dic[i] = [list1.count(i), list2.count(i)]
-    # print(dic)
-    numerator = 0.0
-    for i in dic:
-        a = dic[i][0]
-        b = dic[i][1]
-        numerator = numerator + a*b
-    # print(numerator)
-    s1 = 0.0
-    s2 = 0.0
-    for i in dic:
-        s1 = s1 + (dic[i][0]**2)
-        s2 = s2 + (dic[i][1]**2)
-    sq1 = math.sqrt(s1)
-    sq2 = math.sqrt(s2)
-    denominator = sq1*sq2
-    res = numerator//denominator
-    return res
+    #         dic[i] = [list1.count(i), list2.count(i)]
+    # # print(dic)
+    # numerator = 0.0
+    # for i in dic:
+    #     a = dic[i][0]
+    #     b = dic[i][1]
+    #     numerator = numerator + a*b
+    # # print(numerator)
+    # s1 = 0.0
+    # s2 = 0.0
+    # for i in dic:
+    #     s1 = s1 + (dic[i][0]**2)
+    #     s2 = s2 + (dic[i][1]**2)
+    # sq1 = math.sqrt(s1)
+    # sq2 = math.sqrt(s2)
+    # denominator = sq1*sq2
+    # res = numerator//denominator
+    # return res
+    regex = re.compile('[^a-z]')
+    words1 =[regex.sub("",w.strip()) for w in dict1.lower().split(" ")]
+    words2 =[regex.sub("",w.strip()) for w in dict2.lower().split(" ")]
+
+    dictionary = {}
+    stopwords = load_stopwords("stopwords.txt")
+    for w in words1:
+        if w not in stopwords and len(w)>0:
+            if w not in dictionary.keys():
+                dictionary[w]=[0,0]
+            dictionary[w][0]+=1
+
+
+    for w in words2:
+        if w not in stopwords and len(w)>0:
+            if w not in dictionary.keys():
+                dictionary[w]=[0,0]
+            dictionary[w][1]+=1
+            
+    num = sum([v1*v2 for v1,v2 in dictionary.values()])
+    den1 =  math.sqrt((sum([v1**2 for v1,v2 in dictionary.values()])))
+    den2 =  math.sqrt((sum([v2**2 for v1,v2 in dictionary.values()])))
+
+    return num/(den1*den2)
 
 
 
