146fbe7c19b7b4a1084a69abacae7bd4f3c74ee6
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-18 10:33:20 +0530

    " -> 0 of 1 passed. pylint: 9.36/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -73,11 +73,11 @@ def build_search_index(docs):
 
     # return search index
     stopwords = load_stopwords("stopwords.txt")
-    for i in docs:
-        if i in stopwords:
-            docs.remove(i)
-    dic = {}
     list_1 = docs
+    for i in list_1:
+        if i in stopwords:
+            list_1.remove(i)
+    dic = {}
     list_2 = word_list(docs)
     # print(list_1)
     # print(list_2)
