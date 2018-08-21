af4784b56c1ed3b69dc24e37596086d7a46315f5
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-21 20:15:44 +0530

    " -> 4 of 5 passed. pylint: 8.75/10 "

--- M14/p1/assignment1.py
+++ M14/p1/assignment1.py
@@ -97,7 +97,7 @@ class Message(object):
         shift (integer): the amount by which to shift every letter of the 
         alphabet. 0 <= shift < 26
 
-        Returns: a dictionary mapping a letter (string) to 
+        Returns: a dictionary mapping a letter (string) to
                  another letter (string).
         '''
      #delete this line and replace with your code here
@@ -124,7 +124,7 @@ class Message(object):
         '''
         Applies the Caesar Cipher to self.message_text with the input shift.
         Creates a new string that is self.message_text shifted down the
-        alphabet by some number of characters determined by the input shift        
+        alphabet by some number of characters determined by the input shift
 
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
