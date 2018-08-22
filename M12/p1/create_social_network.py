72bb818bd9da9d1d66f1c13d481a3d76bfe5ed1c
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-22 19:13:57 +0530

    " -> 0 of 3 passed. pylint: 9.33/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -2,7 +2,7 @@
 def create_social_network(data):
     """split"""
     new_data = data.split(" follows ")
-    new_data = data.split("\n")
+    new_data = new_data.split("\n")
     print(new_data)
 
 
