acce30273c3ee2548958d4a5d4dc822ebb7536c3
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-22 19:29:30 +0530

    " -> 0 of 3 passed. pylint: 8.70/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -2,10 +2,19 @@
 def create_social_network(data):
     """split"""
     new_data = data.split('\n')
-    print(data)
+    dic_1 = {}
+    # print(data)
     for i in range(len(new_data)-1):
         data_1 = new_data[i].split(" follows ")
         print(data_1)
+        if len(data_1) <= 1:
+            return dic_1
+        elif new_data[i] in dic_1:
+            dic_1[i[0]].append(dic_1[i][1].split(','))
+        else:
+            dic_1[i[0]] = dic_1[i][1].split(',')
+    return dic_1
+        
 
                 # i = i+1
 '''
