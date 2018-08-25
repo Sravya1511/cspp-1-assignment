100b6598ff94d69652254bd27ec019ecbcbaf3f5
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-25 13:00:56 +0530

    " -> 26 of 26 passed. pylint: 10.00/10 "

--- M22/Check Sudoku/check_sudoku.py
+++ M22/Check Sudoku/check_sudoku.py
@@ -56,6 +56,7 @@ def main():
         # read a line, split it on SPACE and append row to list
         row = input().split(' ')
         sudoku.append(row)
+        i = i+1
     # call solution function and print result to console
     print(check_sudoku(sudoku))
 
